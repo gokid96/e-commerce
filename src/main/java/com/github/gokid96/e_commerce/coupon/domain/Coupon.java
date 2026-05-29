be032709e19cadd2453880d5e983dcbf6e67fcf4
gokid96 <sungmin360@nate.com>
2026-05-29 18:11:28 +0900

    [REFACTOR] Coupon 엔티티 수정

--- src/main/java/com/github/gokid96/e_commerce/coupon/domain/Coupon.java
+++ src/main/java/com/github/gokid96/e_commerce/coupon/domain/Coupon.java
@@ -1,5 +1,6 @@
 package com.github.gokid96.e_commerce.coupon.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -11,60 +12,73 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Coupon {
 
     @Id
+    @Column(name = "coupon_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
-    private long discountAmount;
-    private int totalQuantity;  // 총 발급 가능 수량
-    private int issuedQuantity; // 현재까지 발급된 수량
+    private double discountRate;
+    private int quantity;
 
     @Enumerated(EnumType.STRING)
     private CouponStatus status;
 
+    private LocalDateTime expiredAt;
+
     @Builder
-    private Coupon(Long id, String name, long discountAmount, int totalQuantity) {
+    private Coupon(Long id, String name, double discountRate, int quantity, CouponStatus status, LocalDateTime expiredAt) {
         this.id = id;
         this.name = name;
-        this.discountAmount = discountAmount;
-        this.totalQuantity = totalQuantity;
-        this.issuedQuantity = 0;
-        this.status = CouponStatus.AVAILABLE;
+        this.discountRate = discountRate;
+        this.quantity = quantity;
+        this.status = status;
+        this.expiredAt = expiredAt;
     }
 
-    public static Coupon create(String name, long discountAmount, int totalQuantity) {
-        if (discountAmount <= 0) {
-            throw new IllegalArgumentException("할인 금액은 양수여야 합니다.");
+    public static Coupon create(String name, double discountRate, int quantity, CouponStatus status, LocalDateTime expiredAt) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("쿠폰 이름은 필수입니다.");
         }
-        if (totalQuantity <= 0) {
-            throw new IllegalArgumentException("발급 수량은 양수여야 합니다.");
+        if (discountRate < 0 || discountRate > 1) {
+            throw new IllegalArgumentException("할인율은 0과 1 사이여야 합니다.");
+        }
+        if (quantity < 0) {
+            throw new IllegalArgumentException("쿠폰 수량은 0 이상이어야 합니다.");
+        }
+        if (status == null) {
+            throw new IllegalArgumentException("쿠폰 상태는 필수입니다.");
+        }
+        if (expiredAt == null || expiredAt.isBefore(LocalDateTime.now())) {
+            throw new IllegalArgumentException("쿠폰 만료일은 현재 시간 이후여야 합니다.");
         }
         return Coupon.builder()
                 .name(name)
-                .discountAmount(discountAmount)
-                .totalQuantity(totalQuantity)
+                .discountRate(discountRate)
+                .quantity(quantity)
+                .status(status)
+                .expiredAt(expiredAt)
                 .build();
-
     }
 
     public void issue() {
-        if (status == CouponStatus.UNAVAILABLE) {
+        if (status.cannotPublishable()) {
             throw new IllegalArgumentException("발급 불가능한 쿠폰입니다.");
         }
-        if (issuedQuantity >= totalQuantity) {
-            this.status = CouponStatus.UNAVAILABLE;
+        if (expiredAt.isBefore(LocalDateTime.now())) {
+            throw new IllegalArgumentException("쿠폰이 만료되었습니다.");
+        }
+        if (quantity <= 0) {
             throw new IllegalArgumentException("쿠폰이 모두 소진되었습니다.");
         }
-        this.issuedQuantity++;
-        if (issuedQuantity >= totalQuantity) {
-            this.status = CouponStatus.UNAVAILABLE;
-        }
+        this.quantity--;
     }
 
 }
