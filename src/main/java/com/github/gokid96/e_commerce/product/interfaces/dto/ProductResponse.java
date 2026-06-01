2e45f3f706eb4cef7c3c9f6c263d855f7abcd227
gokid96 <sungmin360@nate.com>
2026-06-01 17:30:13 +0900

    [FEAT] Product Controller/Response 구현 및 mock 정리

---
+++ src/main/java/com/github/gokid96/e_commerce/product/interfaces/dto/ProductResponse.java
@@ -0,0 +1,55 @@
+package com.github.gokid96.e_commerce.product.interfaces.dto;
+
+import com.github.gokid96.e_commerce.product.domain.product.ProductInfo;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ProductResponse {
+
+    @Getter
+    public static class Products {
+        private final List<Product> products;
+
+        private Products(List<Product> products) {
+            this.products = products;
+        }
+
+        public static Products of(ProductInfo.Products info) {
+            List<Product> products = info.getProducts().stream()
+                    .map(Product::of)
+                    .toList();
+            return new Products(products);
+        }
+    }
+
+    @Getter
+    public static class Product {
+        private final Long id;
+        private final String name;
+        private final long price;
+        private final int stock;
+
+        @Builder
+        private Product(Long id, String name, long price, int stock) {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+            this.stock = stock;
+        }
+
+        public static Product of(ProductInfo.Product info) {
+            return Product.builder()
+                    .id(info.getProductId())
+                    .name(info.getProductName())
+                    .price(info.getProductPrice())
+                    .stock(info.getStock())
+                    .build();
+        }
+
+    }
+}
