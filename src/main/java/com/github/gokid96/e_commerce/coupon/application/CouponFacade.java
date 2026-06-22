c7684f3a7e6b10645aa75b1434b412cad0c6737f
gokid96 <sungmin360@nate.com>
2026-06-22 15:13:43 +0900

    [FEAT] 선착순 쿠폰 분산 락 적용

--- src/main/java/com/github/gokid96/e_commerce/coupon/application/CouponFacade.java
+++ src/main/java/com/github/gokid96/e_commerce/coupon/application/CouponFacade.java
@@ -1,10 +1,14 @@
 package com.github.gokid96.e_commerce.coupon.application;
 
+import com.github.gokid96.e_commerce.common.lock.DistributedLock;
+import com.github.gokid96.e_commerce.common.lock.LockStrategy;
+import com.github.gokid96.e_commerce.common.lock.LockType;
 import com.github.gokid96.e_commerce.coupon.domain.CouponInfo;
 import com.github.gokid96.e_commerce.coupon.domain.CouponService;
 import com.github.gokid96.e_commerce.user.domain.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,6 +18,8 @@ public class CouponFacade {
     private final UserService userService;
     private final CouponService couponService;
 
+    @Transactional
+    @DistributedLock(type = LockType.COUPON, key = "#criteria.couponId", strategy = LockStrategy.SPIN_LOCK)
     public void issueCoupon(CouponCriteria.Issue criteria) {
         userService.getUser(criteria.getUserId());
         couponService.issueCoupon(criteria.toCommand());
