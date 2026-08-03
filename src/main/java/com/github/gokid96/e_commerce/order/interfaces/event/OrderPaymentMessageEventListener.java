3d56a3d65dd1309b05702c1811ddc75a520000dc
gokid96 <sungmin360@nate.com>
2026-08-03 15:13:10 +0900

    [FEAT] 주문/결제 관련 카프카 이벤트 리스너 작성

---
+++ src/main/java/com/github/gokid96/e_commerce/order/interfaces/event/OrderPaymentMessageEventListener.java
@@ -0,0 +1,56 @@
+package com.github.gokid96.e_commerce.order.interfaces.event;
+
+import com.github.gokid96.e_commerce.common.event.Event;
+import com.github.gokid96.e_commerce.common.event.EventType.GroupId;
+import com.github.gokid96.e_commerce.common.event.EventType.Topic;
+import com.github.gokid96.e_commerce.order.domain.OrderService;
+import com.github.gokid96.e_commerce.payment.domain.PaymentEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class OrderPaymentMessageEventListener {
+
+    private final OrderService orderService;
+
+    @KafkaListener(topics = Topic.PAYMENT_PAID, groupId = GroupId.ORDER)
+    public void handlePaymentPaid(String message, Acknowledgment ack) {
+        log.info("결제 완료 이벤트 수신 {}", message);
+
+        Event<PaymentEvent.Paid> event = Event.of(message, PaymentEvent.Paid.class);
+        PaymentEvent.Paid payload = event.getPayload();
+
+        orderService.completedOrder(payload.getOrderId());
+
+        ack.acknowledge();
+    }
+
+    @KafkaListener(topics = Topic.PAYMENT_FAILED, groupId = GroupId.ORDER)
+    public void handlePaymentFailed(String message, Acknowledgment ack) {
+        log.info("결제 실패 이벤트 수신 {}", message);
+
+        Event<PaymentEvent.PayFailed> event = Event.of(message, PaymentEvent.PayFailed.class);
+        PaymentEvent.PayFailed payload = event.getPayload();
+
+        orderService.cancelOrder(payload.getOrderId());
+
+        ack.acknowledge();
+    }
+
+    @KafkaListener(topics = Topic.PAYMENT_CANCELED, groupId = GroupId.ORDER)
+    public void handlePaymentCanceled(String message, Acknowledgment ack) {
+        log.info("결제 취소 이벤트 수신 {}", message);
+
+        Event<PaymentEvent.Canceled> event = Event.of(message, PaymentEvent.Canceled.class);
+        PaymentEvent.Canceled payload = event.getPayload();
+
+        orderService.cancelOrder(payload.getOrderId());
+
+        ack.acknowledge();
+    }
+}
---
+++ src/main/java/com/github/gokid96/e_commerce/payment/interfaces/event/PaymentOrderMessageEventListener.java
@@ -0,0 +1,40 @@
+package com.github.gokid96.e_commerce.payment.interfaces.event;
+
+import com.github.gokid96.e_commerce.common.event.Event;
+import com.github.gokid96.e_commerce.common.event.EventType.GroupId;
+import com.github.gokid96.e_commerce.common.event.EventType.Topic;
+import com.github.gokid96.e_commerce.order.domain.OrderEvent;
+import com.github.gokid96.e_commerce.payment.domain.PaymentCommand;
+import com.github.gokid96.e_commerce.payment.domain.PaymentService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class PaymentOrderMessageEventListener {
+
+    private final PaymentService paymentService;
+
+    @KafkaListener(topics = Topic.ORDER_CREATED, groupId = GroupId.PAYMENT)
+    public void handleOrderCreated(String message, Acknowledgment ack) {
+        log.info("주문 생성 이벤트 수신 {}", message);
+
+        Event<OrderEvent.Created> event = Event.of(message, OrderEvent.Created.class);
+        OrderEvent.Created payload = event.getPayload();
+
+        paymentService.payPayment(
+                PaymentCommand.Payment.of(
+                        payload.getOrderId(),
+                        payload.getUserId(),
+                        payload.getUserCouponId(),
+                        payload.getTotalPrice()
+                )
+        );
+
+        ack.acknowledge();
+    }
+}
