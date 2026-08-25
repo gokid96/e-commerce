ab5d563058ca01193e42ab66ec695096a13b9066
gokid96 <sungmin360@nate.com>
2026-08-25 16:53:37 +0900

    [TEST] 멀티모듈 테스트 컴파일 정합

--- service/balance/src/test/java/com/github/gokid96/e_commerce/balance/support/ControllerTestSupport.java
+++ service/balance/src/test/java/com/github/gokid96/e_commerce/balance/support/ControllerTestSupport.java
@@ -1,10 +1,10 @@
 package com.github.gokid96.e_commerce.balance.support;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.databind.ObjectMapper;
 import com.github.gokid96.e_commerce.balance.application.BalanceFacade;
 import com.github.gokid96.e_commerce.balance.interfaces.BalanceController;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
