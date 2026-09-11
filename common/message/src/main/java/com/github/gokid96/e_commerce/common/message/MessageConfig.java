c7288c2e35944b36948f6a8c961fbca549b4e7f9
gokid96 <sungmin360@nate.com>
2026-09-11 11:42:50 +0900

    fix: @EnableAsync 활성화 및 open-in-view 비활성화

--- common/message/src/main/java/com/github/gokid96/e_commerce/common/message/MessageConfig.java
+++ common/message/src/main/java/com/github/gokid96/e_commerce/common/message/MessageConfig.java
@@ -2,8 +2,10 @@ package com.github.gokid96.e_commerce.common.message;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @Configuration
+@EnableAsync
 @ComponentScan(basePackages = {
         "com.github.gokid96.e_commerce.common.message",
         "com.github.gokid96.e_commerce.common.infrastructure"
